d813ecf59ffac54e1e532dad4e7fc63538e2ffd7
Christian Anorga <anorga2990@gmail.com>
2022-07-17 11:28:25 -0700

    Update WeatherService.ts
    process.env needs fix

--- src/services/WeatherService.ts
+++ src/services/WeatherService.ts
@@ -1,6 +1,6 @@
 import {Weather, WeatherLocation} from '../model/Weather';
 
-// const key: string = process.env.REACT_APP_OPEN_WEATHER_API_KEY as string;
+// const key: string = process.env.REACT_APP_OPEN_WEATHER_API_KEY as string needs to be fixed;
 const key: string = 'dcb67be70ed2335d3e1eb8a7ef934976'
 if (key === undefined) {
   throw new Error('No Open Weather API Key defined - ensure you set a variable called REACT_APP_OPEN_WEATHER_API_KEY')
